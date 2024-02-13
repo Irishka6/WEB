1a8d83fb4bf2af86dc940f05a728f12af47baee4
Irina <yaurio@yandex.ru>
2024-02-13 17:16:24 +0500

    Готовимся к миссии

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -9,72 +9,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-
-def return_sample_page():
-    return 'Миссия Колонизация Марса'
-
-@app.route('/index')
-def countdown():
-    return 'И на Марсе будут яблони цвести!'
-
-@app.route('/promotion')
-def promotion():
-    return """<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <title>Привет, Яндекс!</title>
-                  </head>
-                  <body>
-                    <h1>Человечество вырастает из детства.</h1>
-                  </body>
-                  <body>
-                    <h1>Человечеству мала одна планета.</h1>
-                  </body>
-                  <body>
-                    <h1>Мы сделаем обитаемыми безжизненные пока планеты.</h1>
-                  </body>
-                  <body>
-                    <h1>И начнем с Марса!</h1>
-                  </body>
-                  <body>
-                    <h1>Присоединяйся!</h1>
-                  </body>
-                </html>"""
-@app.route('/choice/<planet_name>')
-def image(planet_name):
-    return f'''<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Мое предложение: {planet_name}</h1>
-                    <h2>Эта планета близка к Земле</h2>
-                  </body>
-                    <div class="alert alert-success" role="alert">
-                      На ней много необходимых ресурсов
-                    </div>
-                    <div class="alert alert-secondary" role="alert">
-                      На ней есть вода и атмосфера
-                    </div>
-                    <div class="alert alert-warning" role="alert">
-                      на ней есть небольшое могнитное поле
-                    </div>
-                    <div class="alert alert-danger" role="alert">
-                      Наконец, она очень красива!
-                    </div>
-                </html>'''
+@app.route('/')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('index.html', title=title)
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
