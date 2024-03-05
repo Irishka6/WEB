44071b2b0172d5a717f2058625dc2f2a4d5f48f9
Irina <yaurio@yandex.ru>
2024-03-05 15:30:15 +0500

    Первая работа

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from data import db_session
-from data.users import User
+from data.news import Jobs
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -9,14 +10,14 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/blogs.db")
     db_sess = db_session.create_session()
-    user = User()
-    user.surname = "Scott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = "captain"
-    user.address = "module_1"
-    user.email = "scott_chief@mars.org"
-    db_sess.add(user)
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.start_date = datetime.now()
+    job.is_finished = False
+    db_sess.add(job)
     db_sess.commit()
     #app.run()
 
