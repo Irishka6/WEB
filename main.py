733f3db85a04c4d7830884950450cef2f3de5288
Irina <yaurio@yandex.ru>
2024-03-11 23:06:27 +0500

    Список профессий

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ app = Flask(__name__)
 
 
 
-
-
-@app.route('/')
 @app.route('/')
 @app.route('/index/<title>')
 def index(title):
     return render_template('index.html', title=title)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
