dab2e9cbe815c3d981af862988d66ebf9f527d50
Irina <yaurio@yandex.ru>
2024-03-12 18:30:17 +0500

    Журнал работ

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
 from data.news import Jobs
 from datetime import datetime
@@ -10,17 +10,13 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def main():
     db_session.global_init("db/blogs.db")
     db_sess = db_session.create_session()
-    job = Jobs()
-    job.team_leader = 1
-    job.job = 'deployment of residential modules 1 and 2'
-    job.work_size = 15
-    job.collaborators = '2, 3'
-    job.start_date = datetime.now()
-    job.is_finished = False
-    db_sess.add(job)
     db_sess.commit()
-    #app.run()
-
+    app.run()
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(Jobs).all()
+    return render_template("index.html", news=news)
 
 if __name__ == '__main__':
     main()
