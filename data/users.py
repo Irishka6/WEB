56f5b5b3101a87a7a04677cec390c5496265c45a
Irina <yaurio@yandex.ru>
2024-03-12 18:04:13 +0500

    урок Знакомство с flask-sqlalchemy

--- data/users.py
+++ data/users.py
@@ -2,6 +2,7 @@ import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
 from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
@@ -16,3 +17,9 @@ class User(SqlAlchemyBase):
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
     news = orm.relationship("News", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
